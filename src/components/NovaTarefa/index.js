b28f052f1265abae7b7672cdbf73e97f56087a12
maiconm <maiconm23@gmail.com>
2020-11-29 13:24:37 -0300

    Estilizacao NovaTarefa

--- src/components/NovaTarefa/index.js
+++ src/components/NovaTarefa/index.js
@@ -3,8 +3,24 @@ function NovaTarefa() {
   return (
     <>
       <h1>Nova Tarefa</h1>
-      <input />
-      <button>Adicionar</button>
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Recipient's username"
+          aria-label="Recipient's username"
+          aria-describedby="button-addon2"
+        />
+        <div class="input-group-append">
+          <button
+            class="btn btn-outline-success"
+            type="button"
+            id="button-addon2"
+          >
+            Adicionar
+          </button>
+        </div>
+      </div>
     </>
   )
 }
