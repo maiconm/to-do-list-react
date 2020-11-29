7d9924e02ba5de554d7a7d70b39917959b6ec999
maiconm <maiconm23@gmail.com>
2020-11-29 13:03:35 -0300

    Estilizacao Lista de Tarefas

--- src/components/ToDoList/index.js
+++ src/components/ToDoList/index.js
@@ -3,18 +3,34 @@ function ToDoList() {
   return (
     <>
       <h1>To do list</h1>
-      <ul>
-        <li>
-          <input type="checkbox" />
-          Tarefa 1
+      <ul className="list-group">
+        <li className="list-group-item">
+          <div className="row">
+            <div className="col-10">
+              <input
+                checked
+                type="checkbox"
+              />
+              Tarefa 2
+            </div>
+            <div className="col-2">
+              <button className="btn btn-danger">Excluir</button>
+            </div>
+          </div>
         </li>
-        <li>
-          <input
-            checked
-            type="checkbox"
-          />
-          Tarefa 2
-          <button>Excluir</button>
+        <li className="list-group-item">
+          <div className="row">
+            <div className="col-10">
+              <input
+                checked
+                type="checkbox"
+              />
+              Tarefa 2
+            </div>
+            <div className="col-2">
+              <button className="btn btn-danger">Excluir</button>
+            </div>
+          </div>
         </li>
       </ul>
     </>
